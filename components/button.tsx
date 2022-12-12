02a50021a7b456333c37c37ef371b33263066637
Brian Grosso <bgro63@gmail.com>
2022-12-11 20:00:17 -0500

    type error

--- components/button.tsx
+++ components/button.tsx
@@ -1,9 +1,19 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-export const Button: FC<{ onClick?: () => void; text: string; type?: string; disabled?: boolean }> = ({onClick, text, type, disabled}) => {
-    return (
-        <button className="border-2 rounded-sm border-solid border-current p-2 theme-red w-40 m-auto mt-2" onClick={onClick} type={type} disabled={!!disabled}>
-        {text}
-      </button>
-    )
-}
+export const Button: FC<{
+  onClick?: () => void;
+  text: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  disabled?: boolean;
+}> = ({ onClick, text, type, disabled }) => {
+  return (
+    <button
+      className="border-2 rounded-sm border-solid border-current p-2 theme-red w-40 m-auto mt-2"
+      onClick={onClick}
+      type={type}
+      disabled={!!disabled}
+    >
+      {text}
+    </button>
+  );
+};
