b485e12498f800021bc3343412fee31ccee0dc44
Brian Grosso <bgro63@gmail.com>
2022-12-11 21:40:15 -0500

    format

--- components/form-field.tsx
+++ components/form-field.tsx
@@ -1,10 +1,23 @@
 import { FC } from "react";
 import { Field, ErrorMessage } from "formik";
-export const FormField: FC<{ date?: boolean; fieldName: string; placeholder: string }> = ({fieldName, placeholder, date}) => {
-    return (
-        <div className='mx-auto my-1 w-3/4 md:w-stretch'>
-            <Field className="border w-full border-black rounded-sm indent-1 p-1 focus-visible:outline-custom-red" type={(date ? 'date': 'text')} name={fieldName} placeholder={placeholder} />
-            <ErrorMessage name={fieldName} component="div" className="text-custom-red text-xs"  />
-        </div>
-    )
-}
+export const FormField: FC<{
+  date?: boolean;
+  fieldName: string;
+  placeholder: string;
+}> = ({ fieldName, placeholder, date }) => {
+  return (
+    <div className="mx-auto my-1 w-3/4 md:w-stretch">
+      <Field
+        className="border w-full border-black rounded-sm indent-1 p-1 focus-visible:outline-custom-red"
+        type={date ? "date" : "text"}
+        name={fieldName}
+        placeholder={placeholder}
+      />
+      <ErrorMessage
+        name={fieldName}
+        component="div"
+        className="text-custom-red text-xs"
+      />
+    </div>
+  );
+};
